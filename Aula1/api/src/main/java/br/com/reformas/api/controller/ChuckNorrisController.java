c0a016c7767fc808504d9fdf8f3cbd94a713e07b
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-08-04 23:15:08 -0300

    Desafio/Teste para fixar conteudo

--- Aula1/api/src/main/java/br/com/reformas/api/controller/ChuckNorrisController.java
+++ Aula1/api/src/main/java/br/com/reformas/api/controller/ChuckNorrisController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
-
+@RequestMapping("/chuck")
+public class ChuckNorrisController {
+	
 	@GetMapping
-	public String olaMundo() {
-		return "Hello World!!";
+	public String chuckNorrisHello() {
+		return "Hello Chuck Norris!";
 		
 	}
 }
--- Aula1/api/src/main/java/br/com/reformas/api/controller/UniverseController.java
+++ Aula1/api/src/main/java/br/com/reformas/api/controller/UniverseController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
+@RequestMapping("/universe")
+public class UniverseController {
 
 	@GetMapping
-	public String olaMundo() {
-		return "Hello World!!";
+	public String universeHello() {
+		return "Hello Universe!";
 		
 	}
 }
